84b3d860d58743784ef207931dcb695fd284b9a6
Morten Kristensen <msk@nullpointer.dk>
2015-09-26 10:45:17 +0200

    Implemented taggable slots and disconnection.
    A tagged slots can be disconnected using the tag,
    e.g. signal.disconnect("the tag").

--- Signal.h
+++ Signal.h
@@ -1,45 +1,67 @@
 #ifndef SIGS_SIGNAL_H
 #define SIGS_SIGNAL_H
 
+#include <tuple>
 #include <mutex>
+#include <string>
 #include <vector>
 #include <utility>
 
 namespace sigs {
-  template <typename Slot>
+  template <typename Slot, typename Tag = std::string>
   class Signal {
-    using Cont = std::vector<Slot>;
+    using Entry = std::tuple<Slot, Tag>;
+    using Cont = std::vector<Entry>;
     using Lock = std::lock_guard<std::mutex>;
 
   public:
     using SlotType = Slot;
 
-    void connect(const Slot &slot) {
-      Lock lock(slotsMutex);
-      slots.emplace_back(slot);
+    void connect(const Slot &slot, const Tag &tag = Tag()) {
+      Lock lock(entriesMutex);
+      entries.emplace_back(Entry(slot, tag));
     }
 
-    void connect(Slot &&slot) {
-      Lock lock(slotsMutex);
-      slots.emplace_back(std::move(slot));
+    void connect(Slot &&slot, const Tag &tag = Tag()) {
+      Lock lock(entriesMutex);
+      entries.emplace_back(Entry(std::move(slot), tag));
     }
 
-    void disconnect() {
-      Lock lock(slotsMutex);
-      slots.clear();
+    void disconnect(const Tag &tag = Tag()) {
+      Lock lock(entriesMutex);
+
+      if (tag == Tag()) {
+        entries.clear();
+        return;
+      }
+
+      auto end = entries.end();
+      for (auto it = entries.begin(); it != end; it++) {
+        if (getTag(*it) == tag) {
+          entries.erase(it);
+        }
+      }
     }
 
     template <typename ...Args>
     void operator()(Args &&...args) {
-      Lock lock(slotsMutex);
-      for (auto &slot : slots) {
-        slot(std::forward<Args>(args)...);
+      Lock lock(entriesMutex);
+      for (auto &entry : entries) {
+        getSlot(entry)(std::forward<Args>(args)...);
       }
     }
 
   private:
-    Cont slots;
-    std::mutex slotsMutex;
+    inline const Slot &getSlot(const Entry &entry) const {
+      return std::get<0>(entry);
+    }
+
+    inline const Tag &getTag(const Entry &entry) const {
+      return std::get<1>(entry);
+    }
+
+    Cont entries;
+    std::mutex entriesMutex;
   };
 }
 
